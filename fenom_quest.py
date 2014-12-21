648f4a1921e11c2f070c55fa1d40097124df402f
Fengyang Wang <fengyang.wang.0@gmail.com>
2014-12-20 20:07:36 -0500

    Allow shifting screen left and right.

--- fenom_quest.py
+++ fenom_quest.py
@@ -10,7 +10,7 @@ import random
 import pygame
 import character
 
-VERSION = "0.00.16.2"
+VERSION = "0.00.16.4"
 GAME_NAME = "Fenom-Quest"
 
 # INITIALIZATION
@@ -42,9 +42,15 @@ for i in range(character.MAX_LEVEL):
 LEVELUP = pygame.mixer.Sound("music/sfx/levelup.ogg")
 SOUNDTRACK = pygame.mixer.Sound("music/digging-for-riches.ogg")
 
-# Map size.
+# Map and screen size.
 MAP_WIDTH = 11
 MAP_HEIGHT = 11
+SCREEN_WIDTH = 11 # Only odd screen widths/heights supported!
+SCREEN_HEIGHT = 11
+MAX_VIEW_CENTRE_X = MAP_WIDTH - SCREEN_WIDTH // 2 - 1
+MIN_VIEW_CENTRE_X = SCREEN_WIDTH // 2
+MAX_VIEW_CENTRE_Y = MAP_HEIGHT - SCREEN_HEIGHT // 2 - 1
+MIN_VIEW_CENTRE_Y = SCREEN_HEIGHT // 2
 
 # Character start location.
 START_LOCATION = 5, 5
@@ -71,38 +77,56 @@ class Game:
         self.protagonist = character.Character(*START_LOCATION)
 
         # Game view.
-        self.view_center = START_LOCATION
+        self.view_centre = START_LOCATION
+
+    def view_coordinates(self, actual_x, actual_y):
+        """Given actual coordinates, calculate coordinates after shifting for
+           the view and multiplying by screen size."""
+        return (64 * (actual_x - self.view_centre[1] + SCREEN_WIDTH // 2),
+                64 * (actual_y - self.view_centre[0] + SCREEN_HEIGHT // 2))
 
     def draw_terrain(self):
         """Draws background."""
         # 1. Beach.
-        for tiley in range(MAP_HEIGHT):
-            for tilex in range(MAP_WIDTH):
+        for tiley in range(SCREEN_HEIGHT):
+            for tilex in range(SCREEN_WIDTH):
                 SCREEN.blit(BEACH, (tilex*64, tiley*64))
 
         # 2. Ice.
         for icex, icey in self.ice_locs:
-            SCREEN.blit(ICE, (icex*64, icey*64))
+            SCREEN.blit(ICE, self.view_coordinates(icex, icey))
 
         # 3. Draw the houses.
         for housex, housey in self.house_locs:
-            SCREEN.blit(HOUSE, (housex*64, housey*64))
+            SCREEN.blit(HOUSE, self.view_coordinates(housex, housey))
 
     def draw_characters(self):
         """Draws characters."""
         player = self.protagonist
-        SCREEN.blit(PLAYER[player.level], (player.x * 64, player.y * 64))
+        SCREEN.blit(
+            PLAYER[player.level],
+            self.view_coordinates(player.x, player.y)
+            )
 
     def passable(self, x, y):
         """Returns whether the player is allowed to be in its square."""
         return (x, y) not in self.house_locs \
-               and 0 <= x <= 10 and 0 <= y <= 10
+               and 0 <= x < MAP_WIDTH and 0 <= y < MAP_HEIGHT
+
+    def recalculate_view_centre(self):
+        """Recalculates the optimal centre of the screen for viewing."""
+        player = self.protagonist
+        self.view_centre = (
+            max(MIN_VIEW_CENTRE_X, min(MAX_VIEW_CENTRE_X, player.x)),
+            max(MIN_VIEW_CENTRE_Y, min(MAX_VIEW_CENTRE_Y, player.y))
+            )
 
     def move_up(self):
         """Moves the protagonist North. Returns false on failure."""
         player = self.protagonist
         if self.passable(player.x, player.y - 1):
             player.y -= 1
+            self.recalculate_view_centre()
             return True
         else:
             return False
@@ -112,6 +136,7 @@ class Game:
         player = self.protagonist
         if self.passable(player.x, player.y + 1):
             player.y += 1
+            self.recalculate_view_centre()
             return True
         else:
             return False
@@ -121,6 +146,7 @@ class Game:
         player = self.protagonist
         if self.passable(player.x - 1, player.y):
             player.x -= 1
+            self.recalculate_view_centre()
             return True
         else:
             return False
@@ -130,6 +156,7 @@ class Game:
         player = self.protagonist
         if self.passable(player.x + 1, player.y):
             player.x += 1
+            self.recalculate_view_centre()
             return True
         else:
             return False
