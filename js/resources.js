5d48e40da3b8ec2140576c13aeeaf50a6cb4d7b7
Fengyang Wang <fengyang.wang.0@gmail.com>
2014-12-31 19:21:37 -0500

    Some fiddling with resources.js.

--- js/resources.js
+++ js/resources.js
@@ -1,43 +1,53 @@
-'''Contains resources and Pygame objects needed for FLUKE RPG.'''
+// Contains resources and Pygame objects needed for VenomQuest
+var res = {};
 
-import pygame
+res.loader = function() {
+  function load_image(id) {
+    return document.getElementById(id);
+  }
 
-pygame.init()
-pygame.mixer.init()
-pygame.display.set_mode()
-
-# Terrain images.
-BEACH = pygame.image.load("art/beach.png").convert_alpha()
-WATER = pygame.image.load("art/water.png").convert_alpha()
-HOUSE = pygame.image.load("art/house.png").convert_alpha()
-TREE = pygame.image.load("art/tree.png").convert_alpha()
-
-# Decoration images.
-DECORATIONS = {
-    "attack": pygame.image.load("art/cat3.png").convert_alpha(),
-    "corpse": pygame.image.load("art/corpse.png").convert_alpha()
+  window.res = {
+    image: {
+      attack: load_image("im-attack"),
+      beach: load_image("im-beach"),
+      corpse: load_image("im-corpse"),
+      house: load_image("im-house"),
+      water: load_image("im-water"),
+      tree: load_image("im-tree")
+    },
+    creature: {
+      fighter: {
+        to: 7
+      },
+      steven: {
+        to: 1
+      },
+      sierra: {
+        to: 1
+      },
+      rabbit: {
+        to: 1
+      }
+    },
+    sfx: {
+      hit: document.getElementById("sfx-hit"),
+      levelup: document.getElementById("sfx-levelup")
     }
+  };
 
-# Player images.
-MAX_LEVEL = 5
-PLAYER = {}
-for i in range(MAX_LEVEL):
-    PLAYER[i+1] = pygame.image.load(
-        "art/fighter{}.png".format(i+1)
-        ).convert_alpha()
-
-# Enemy images.
-ENEMY_IMAGES = {
-    "steven": pygame.image.load("art/male-head.png"),
-    "sierra": pygame.image.load("art/female-head.png"),
-    "rabbit": pygame.image.load("art/rabbitside.png")
+  // Load player images.
+  for (var creature in res.creature) {
+    for (var i = 1; i <= res.creature[creature].to; i++) {
+      res.creature[creature][i] = document.getElementById(
+        "ip-" + creature + "-" + i
+        );
     }
+  }
+};
+window.addEventListener("load", res.loader);
 
-# Sound cache.
-SOUNDTRACK = pygame.mixer.Sound("music/digging-for-riches.ogg")
-SFX_HIT = pygame.mixer.Sound("music/sfx/hit.ogg")
-SFX_LEVELUP = pygame.mixer.Sound("music/sfx/levelup.ogg")
-
+/*
 # Fonts.
 SMALL_FONT = pygame.font.Font(None, 14)
 LARGE_FONT = pygame.font.Font(None, 20)
+* */
